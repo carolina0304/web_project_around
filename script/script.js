616beb0b422b019834c9cfe8da86918d3bdd5279
carolina0304 <garlene122@gmail.com>
2025-03-13 17:45:16 -0600

    agregar script guardar

--- script/script.js
+++ script/script.js
@@ -11,3 +11,11 @@ const buttonclose = document.querySelector(".popup__close");
 buttonclose.addEventListener("click", () => {
   popup.classList.toggle("popup_invisible");
 });
+
+const formElement = document.querySelector(".popup__button");
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  let nameInput = document.querySelector(".popup__input");
+  let jobInput = document.querySelector("profile__name_names");
+}
+formElement.addEventListener("submit", handleProfileFormSubmit);
