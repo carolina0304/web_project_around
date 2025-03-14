dab407dbeab7e54d247b117295cde9d8e33a7ce1
carolina0304 <garlene122@gmail.com>
2025-03-14 14:59:24 -0600

    corregir bem

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 const popupButton = document.querySelector(".popup__button");
-const button1 = document.querySelector(".profile__name_editbutton");
+const button1 = document.querySelector(".profile__nameeditbutton");
 const popup = document.querySelector("#popup1");
 const inputname = document.querySelector("#name2");
 const inputlastname = document.querySelector("#name3");
 
-const profilename = document.querySelector(".profile__name_names");
-const profilesubname = document.querySelector(".profile__name_subname");
+const profilename = document.querySelector(".profile__namenames");
+const profilesubname = document.querySelector(".profile__namesubname");
 
 const popupclose = document.querySelector(".popup__close");
 
