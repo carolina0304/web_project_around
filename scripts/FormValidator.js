c83d2760c1314cd04287cec12671739672ab4807
carolina0304 <garlene122@gmail.com>
2025-04-29 17:17:06 -0600

    formvalidator datos

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,3 +1,16 @@
 class FormValidator {
-  this.popupForms = document.querySelectorAll(".popup__form");
+  constructor(formSelector) {
+    this.popupForms = document.querySelectorAll(formSelector);
+    const popupForms = document.querySelectorAll(".popup__form");
+  }
+
+  hello() {
+    console.log("este es metodo publico");
+    _helloprivado();
+  }
+  _helloprivado() {
+    console.log("este es metodo privado");
+  }
 }
+
+export default FormValidator;
