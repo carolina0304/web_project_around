36348a5cc0faf6e2c360fe0811b80e9eb75dbac5
carolina0304 <garlene122@gmail.com>
2025-06-04 18:34:39 -0600

    Acomodar section

--- components/Section.js
+++ components/Section.js
@@ -3,15 +3,23 @@
 //para renderizar los elementos de la pagina
 export default class Section {
   constructor({ items, renderer }, sectionSelector) {
+    //items es un array de elementos de datos,
+    //renderer es una funcion sera responsable de renderizar los datos.
     this._items = items;
     this._renderer = renderer;
+    //container se van agregar los elementos renderizados.
     this._container = sectionSelector;
   }
+
+  //Metodo para renderizar todos los elementos de la lista.
   renderItem() {
+    //Se renderiza cada item de la pagina.
     this._items.forEach((item) => {
-      const elemento = this._renderer(item);
+      this._renderer(item);
     });
   }
+
+  //Metodo agrega un nuevo elemento al contenedor.
   addItem(elemento) {
     console.log(elemento);
     this._container.prepend(elemento);
